fc172a6cabff92a01def05974dfe59c787cb0b89
Rossignol Clara <rossigno@INTRANET.EPFL.CH@SVUbuUniv8.intranet.epfl.ch>
2020-11-03 19:42:02 +0100

    a few constants

--- src/constant.h
+++ src/constant.h
@@ -1,1 +1,15 @@
-// To complete
+
+/// * default parameter values *
+#define _AVG_NUMBER_ 10.
+#define _AVG_PROP_ 0.8.
+#define _END_TIME_ 10
+#define _AVG_CNNCT_ 3 //am really not sure about this one, pretty much invented 
+#define _AVG_INTENSITY_ 2 //same thing with this one
+
+/// * text messages *
+#define _PRGRM_TEXT_ "Simple model of spiking neurons - neuron network"
+#define _NUMBER_TEXT_ "Number of neurons in simulation"
+#define _PROP_TEXT_ "Proportion of excitatory neurons"
+#define _TIME_TEXT_ "Duration of simulation (in number of steps)"
+#define _CNNCT_TEXT_ "Average connectivity"
+#define _INTENSITY_TEXT_ "Average connections' intensity"
