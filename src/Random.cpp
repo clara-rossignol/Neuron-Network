eb24bd9ab3bc4790e96b84ef820ed5e975a6f2c3
Rossignol Clara <rossigno@INTRANET.EPFL.CH@SVUbuUniv9.intranet.epfl.ch>
2020-11-03 18:51:50 +0100

    class Random : some functions that might be needed for the project, not final

--- src/Random.cpp
+++ src/Random.cpp
@@ -1,1 +1,28 @@
-//To complete
+#include "Random.h"
+
+RandomNumbers::RandomNumbers(unsigned long int s) 
+:seed(s)
+{
+	if (seed == 0) {
+		std::random_device rd;
+		seed = rd();
+	}
+	rng.seed(seed);
+}
+
+double RandomNumbers::uniform_double(double lower, double upper) {
+	std::uniform_real_distribution<> unif(lower, upper);
+	return unif(rng);
+}
+
+double RandomNumbers::normal(double mean, double sd)
+{
+	std::normal_distribution<> norm(mean, sd);
+	return norm(rng);
+}
+
+int RandomNumbers::poisson(double mean)
+{
+	std::poisson_distribution<> poiss(mean);
+	return poiss(rng);
+}
--- src/Random.h
+++ src/Random.h
@@ -1,1 +1,39 @@
-//To complete
+#ifndef RANDOM_H
+#define RANDOM_H
+
+#include <random>
+#include <vector>
+#include <algorithm>
+
+/*!
+  This is a random number class based on standard c++-11 generators.
+*/
+
+class RandomNumbers {
+
+public:
+/*! @name Initializing
+  The generator \ref rng is a Mersenne twister *mt19937* engine. 
+  A seed *s>0* can be provided, by default it is seeded with a *random_device*.
+*/
+///@{
+    RandomNumbers(unsigned long int s=0);
+///@}
+/*! @name Distributions
+  These functions return a single number.
+*/
+///@{
+    double uniform_double(double lower, double upper);
+    double normal(double mean, double sd);
+    int poisson(double mean);
+///@}
+
+private:
+    std::mt19937 rng;
+    long int seed;
+
+};
+
+extern RandomNumbers *_RNG;
+
+#endif //RANDOM_H
