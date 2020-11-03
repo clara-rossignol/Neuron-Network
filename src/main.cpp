e771405afe967f86a5379da786ddd9996d4a1c89
Rossignol Clara <rossigno@INTRANET.EPFL.CH@SVUbuUniv2.intranet.epfl.ch>
2020-11-03 23:25:42 +0100

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
+#include "constants.h"
+#include "Random.h"
+#include "Simulation.h"
 
-int main(int argc, char **argv {
-	
-	
-	return 0;
+//to be modified/completed
+RandomNumbers *_RNG;
+
+int main(int argc, char **argv) {    
+    try {
+        Simulation s(argc, argv);
+        s.run();
+    } catch(SimulError &e) {
+        std::cerr << e.what() << std::endl;
+        return e.value();
+    }
+    if (_RNG) delete _RNG;
+    return 0;
 }
