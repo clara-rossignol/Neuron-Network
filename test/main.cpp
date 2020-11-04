4fe6e8633b8fad05e8f3ffff8cbee4856814a33f
Alexandra Preda <alexandra-elena.preda@epfl.ch>
2020-11-04 19:10:54 +0100

    few changes to the main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -1,1 +1,10 @@
 //tests
+//this will be the main that will unite all the tests
+
+#include <gtest/gtest.h>
+
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
