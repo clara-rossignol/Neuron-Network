8368f135cc0423cc3adeb99cdb25a9479adc8c8a
Rossignol Clara <rossigno@INTRANET.EPFL.CH@SVUbuUniv15.intranet.epfl.ch>
2020-11-08 17:23:56 +0100

    trying to solve issues, not finished

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -9,11 +9,11 @@ Simulation::Simulation(int argc, char **argv) {
         cmd.add(total_n);
         TCLAP::ValueArg<double> pE("pE", "excitatory neurons", _PROP_TEXT_, false, _AVG_PROP_, "double");
         cmd.add(pE);
-        TCLAP::ValueArg<int> maxt("t", "time", _TIME_TEXT_, false, _END_TIME_, "int");
+        TCLAP::ValueArg<int> maxt("t", "time", _TIME_TEXT_, false, _TIME_, "int");
         cmd.add(maxt);
         TCLAP::ValueArg<double> connectivity("λ", "connectivity", _CNNCT_TEXT_, false, _AVG_CNNCT_, "double");
         cmd.add(connectivity);
-        TCLAP::ValueArg<double> intensity("L", "intensity", _INTENSITY_TEXT_, false, _AVG_INTENTITY_, "double");
+        TCLAP::ValueArg<double> intensity("L", "intensity", _INTENSITY_TEXT_, false, _AVG_INTENSITY_, "double");
         cmd.add(intensity);
         //not sure if this is needed
         TCLAP::ValueArg<long> seed("S", "seed", "Random seed", false, 0, "long");
@@ -22,12 +22,22 @@ Simulation::Simulation(int argc, char **argv) {
         //not sure if this is needed
         _RNG = new RandomNumbers(seed.getValue());
         
-        _neurons = total_n.getValue();
+        _size = total_n.getValue();
         _pE = pE.getValue();
-        _end_time = maxt.getValue();
+        _endtime = maxt.getValue();
         _connectivity = connectivity.getValue();
         _intensity = intensity.getValue();
+        
+} catch(TCLAP::ArgException &e) {
+	throw(TCLAP_ERROR("Error: " + e.error() + " " + e.argId()));
+}
+}
+
+void Simulation::run(){
+	//to be written
+	
 }
 
 Simulation::~Simulation() {
+
 }
