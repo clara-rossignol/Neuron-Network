b3070b2af67b0541de093be9813ef16ba343dd97
Rossignol Clara <rossigno@INTRANET.EPFL.CH@SVUbuUniv5.intranet.epfl.ch>
2020-11-29 23:59:17 +0100

    documentation

--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,18 @@
 This is an implementation of the model of E.M. Izhikevich
 ([Simple Model of Spiking Neuron, IEE Trans. Neural Net., 2003] (https://www.izhikevich.org/publications/spikes.pdf)).
 
+  @authors  Alexandra-Elena Preda <alexandra-elena.preda@epfl.ch> <br>
+* 			Clara Rossignol <clara.rossignol@epfl.ch> <br>
+* 			Constance Laure Marie Géraldine Gabrielle De Trogoff Coatallio <constance.detrogoff@epfl.ch> <br>
+* 			Lola Maïa Lou Bardel <lola.bardel@epfl.ch>
+
  The given command is: 
   \verbatim
   ./NeuronNetwork -T 'FS:0.2,IB:0.15,CH:0.15,RS:0.5' -t 500 -N 10000
   \endverbatim
-  
-  
 
 */
-//to be modified/completed
+
 RandomNumbers *_RNG;
 
 int main(int argc, char **argv) 
@@ -26,7 +29,6 @@ int main(int argc, char **argv)
   /*
   / This is for the programm to write the errors that happened 
   */
-
   std::atexit(&Error::write);
     _RNG = new RandomNumbers();
 
