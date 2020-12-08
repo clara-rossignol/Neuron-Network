6841520d7e310d8add6a01368e34c3b417e431aa
Rossignol Clara <rossigno@INTRANET.EPFL.CH@SVUbuUniv6.intranet.epfl.ch>
2020-12-08 18:57:16 +0100

    documentation

--- src/DispNetwork.h
+++ src/DispNetwork.h
@@ -7,21 +7,25 @@
    \brief This is another type of \ref Network.
    
    This is an extension, representing an overdispersed network:
-   for each neuron the connection will be set to an exponential(1/connectivity), where exponential is a method of \ref RandomNumbers
+   for each neuron the connection will be set to an exponential(1/connectivity), where exponential is a method of \ref RandomNumbers.
  */
 class DispNetwork : public Network
 {
 public:
+	/*! \name Constructors
+	 */
+///@{
     DispNetwork(size_t s, TypesProportions prop);
 /** Initialises the network with neurons
-   @param neurons: a set of neurons
+   @param neurons: a set of neurons (vector<Neuron>)
    */
     DispNetwork(const std::vector<Neuron> &neurons);
+///@}
 
     /*!
     Sets the connections, as explained in the description of the class.
-    @param meanIntensity the mean intensity of a connection
-    @param meanConnectivity the mean number of entering connections in one neuron
+    @param meanIntensity the mean intensity of a connection (double)
+    @param meanConnectivity the mean number of entering connections in one neuron (double)
     */
     void setConnections(double meanIntensity, double meanConnectivity) override;
 };
