219859cfe6621455a580ada5ef5c5039790043ea
Lola Maïa Lou Bardel <lola.bardel@epfl.ch>
2020-12-13 16:22:30 +0100

    Clean code

--- src/Network.h
+++ src/Network.h
@@ -17,67 +17,70 @@ public:
 /*! \name Initialization
  */
 ///@{
-	/*!
-   \param neurons: all neurons that will form the network
+  /*!
+   * \param neurons: all neurons that will form the network
    */
    Network(const std::vector<Neuron>& neurons);
 
-   /*! A set of neurons of size *size* is created according to proportions given in *prop*. 
-    * The last position in the network of each type of neurons is recorded in \ref _indexes in order to print neuron type specific values in output files.
-   \param size (size_t): the size of the network
-   \param prop (TypesProportions): all proportions corresponding to each type of neurons
+  /*! A set of neurons of size *size* is created according to proportions given in *prop*. 
+   * The last position in the network of each type of neurons is recorded in \ref _indexes in order to print neuron type specific values in output files.
+   * \param size (size_t): the size of the network
+   * \param prop (TypesProportions): all proportions corresponding to each type of neurons
    */
    Network(size_t size, const TypesProportions& prop);
    
-   /*! \name Destructor
- */
+  /*! \name Destructor
+   */
     virtual ~Network() = default;
 ///@}
 
 	/*!
-     * *setConnections* makes the entering connections for all neurons in the network.
-     \param meanIntensity: the mean intensity of a connection 
-     \param meanConnectivity: the mean number of entering connections in one neuron 
-     */
+   * *setConnections* makes the entering connections for all neurons in the network.
+   * \param meanIntensity: the mean intensity of a connection 
+   * \param meanConnectivity: the mean number of entering connections in one neuron 
+   */
     virtual void setConnections(double meanIntensity, double meanConnectivity);
     
-    /*!
-     *setNeuronConnections* makes the entering connections for one neuron
-     Creates a set of inhibitory senders and a set of excitatory senders that will be assembled
-     in neuron \ref setConnections. Those two ensembles have been created in order to facilitate
-     \ref Neuron::currentCalculation in Neuron and \ref print_samples
-     \param meanIntensity: the mean intensity of a connection 
-     \param meanConnectivity: the mean number of entering connections in one neuron 
-     \param neuron: the neuron for which connections are set
-     */
+  /*!
+   * *setNeuronConnections* makes the entering connections for one neuron
+   * Creates a set of inhibitory senders and a set of excitatory senders that will be assembled
+   * in neuron \ref setConnections. Those two ensembles have been created in order to facilitate
+   * \ref Neuron::currentCalculation in Neuron and \ref print_samples
+   * \param meanIntensity: the mean intensity of a connection 
+   * \param meanConnectivity: the mean number of entering connections in one neuron 
+   * \param neuron: the neuron for which connections are set
+   */
     virtual void setNeuronConnections(double meanIntensity, double meanConnectivity, Neuron& neuron);
 
-    /*!
-     Sets all neurons behavior during the simulation
-     In order to have all firing neurons influencing the current at the same time,
-     the \ref _firing of all neurons is updated separately
-     */
+  /*!
+   * Sets all neurons behavior during the simulation
+   * In order to have all firing neurons influencing the current at the same time,
+   * the \ref _firing of all neurons is updated separately
+   */
     void update(double thal);
     
-    /*! \name Output
-     * These methods will print the different output files.
-    * \param *_outstr: an ostream
-    */
+  /*! \name Output
+   * These methods will print the different output files.
+   * \param *_outstr: an ostream
+   */
     ///@{
-    /*! Prints the different parameters \p a, \p b, \p c, \p d, if the neuron is \p inhibitory or not, the \p degree (number of connections towards the neuron) and the \p valence (sum of all connections' intensities).
-     */
-    void print_params(std::ostream *_outstr);
-    /*! Prints the \ref _membranePotential, \ref _recoveryVariable and \ref _current from 1 neuron of each type present in the Network.
-     */
-    void print_sample(std::ostream *_outstr);
-    /*! Prints a matrix with N columns (N = number of neurons in the Network) and T lines (T = number of steps in the simulation) with the value 1 if a neuron is firing and 0 otherwise.
-     */
-    void print_spikes(std::ostream *_outstr);
+  /*! 
+   * Prints the different parameters \p a, \p b, \p c, \p d, if the neuron is \p inhibitory or not, the \p degree (number of connections towards the neuron) and the \p valence (sum of all connections' intensities).
+   */
+  void print_params(std::ostream *_outstr);
+  /*! 
+   * Prints the \ref _membranePotential, \ref _recoveryVariable and \ref _current from 1 neuron of each type present in the Network.
+   */
+  void print_sample(std::ostream *_outstr);
+  /*! 
+   * Prints a matrix with N columns (N = number of neurons in the Network) and T lines (T = number of steps in the simulation) with the value 1 if a neuron is firing and 0 otherwise.
+   */
+  void print_spikes(std::ostream *_outstr);
     ///@}
     
-    /*!
-     It returns the neurons.
-     * \return _neurons (vector<Neuron>)
+  /*!
+  It returns the neurons.
+  * \return _neurons (vector<Neuron>)
      */
     const std::vector<Neuron> &getNeurons() const;
     /*!
@@ -89,8 +92,8 @@ public:
 
 protected:
  /*!
-     A set of neurons
-     */
+  * A set of neurons
+  */
     std::vector<Neuron> _neurons;
     std::vector<int> _indexes;
 };
--- src/Random.h
+++ src/Random.h
@@ -6,25 +6,27 @@
 #include <algorithm>
 
 /*! \class RandomNumbers
-    \brief This class generates random numbers.
-    
-    This is a random number class based on standard c++-11 generators.
-*/
+ *  \brief This class generates random numbers.
+ *  
+ *  This is a random number class based on standard c++-11 generators.
+ */
 
-class RandomNumbers {
-
+class RandomNumbers 
+{
 public:
 /*! \name Initialization
-  The generator rng is a Mersenne twister *mt19937* engine. 
-  A seed *s>0* can be provided, by default it is seeded with a *random_device*.
-*/
+ */
+///@{
+/*! The generator rng is a Mersenne twister *mt19937* engine. 
+ *  A seed *s>0* can be provided, by default it is seeded with a *random_device*.
+ */
 ///@{
     RandomNumbers(unsigned long int s=0);
 ///@}
 
 /*! \name Distributions
-  These functions return a single number.
-*/
+ *  These functions return a single number.
+ */
 ///@{
     double uniform_double(double lower, double upper);
     int uniform_int(double lower, double upper);
